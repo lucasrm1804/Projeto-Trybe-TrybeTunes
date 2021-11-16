96171f3732cbaac88002b6d4f9edf7fbc9b8244f
Lucas Monteiro <lucasribeirom@hotmail.com>
2021-11-16 18:54:20 -0300

    Requisito 6 - requisição para pesquisar artistas

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from '../Components/Carregando';
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       singerName: '',
+      searchName: '',
       buttonDisabled: true,
+      singerInfos: [],
+      verifyApi: true,
+      returnMsg: '',
+      showResult: false,
     };
   }
 
   getSinger = (event) => {
     const { value } = event.target;
     this.setState({ singerName: value }, () => {
-      this.setState({ buttonDisabled: this.disabledButton() });
+      this.setState({
+        buttonDisabled: this.disabledButton(),
+        searchName: value,
+      });
     });
   }
 
@@ -26,33 +37,91 @@ class Search extends React.Component {
     return true;
   }
 
+  getInfoArtista = (event) => {
+    event.preventDefault();
+    const { searchName } = this.state;
+    this.setState({
+      singerName: '',
+      verifyApi: false,
+    }, () => {
+      searchAlbumsAPI(searchName).then((data) => {
+        this.setState({
+          singerInfos: [...data],
+          verifyApi: true,
+          showResult: true,
+        }, () => this.showMsg());
+      });
+    });
+  }
+
+  showMsg = () => {
+    const { singerInfos, searchName } = this.state;
+    if (singerInfos.length > 0) {
+      this.setState({ returnMsg: `Resultado de álbuns de: ${searchName}` });
+    } else {
+      this.setState({ returnMsg: 'Nenhum álbum foi encontrado' });
+    }
+  }
+
   render() {
-    const { buttonDisabled } = this.state;
+    const {
+      buttonDisabled,
+      verifyApi,
+      returnMsg,
+      showResult,
+      singerInfos,
+    } = this.state;
 
     return (
-      <div data-testid="page-search">
-        <Header />
-        Pagina de Search
-        <form>
-          <label htmlFor="search-artist-input">
-            Login
-            <input
-              type="text"
-              data-testid="search-artist-input"
-              minLength="2"
-              onChange={ this.getSinger }
-            />
-          </label>
-          <button
-            disabled={ buttonDisabled }
-            data-testid="search-artist-button"
-            type="submit"
-            onClick={ this.getSinger }
-          >
-            Entrar
-          </button>
-        </form>
-      </div>
+      <>
+        <div data-testid="page-search">
+          <Header />
+          Pagina de Search
+          {
+            verifyApi
+              ? (
+                <form>
+                  <label htmlFor="search-artist-input">
+                    Buscar
+                    <input
+                      type="text"
+                      data-testid="search-artist-input"
+                      minLength="2"
+                      onChange={ this.getSinger }
+                    />
+                  </label>
+                  <button
+                    disabled={ buttonDisabled }
+                    data-testid="search-artist-button"
+                    type="submit"
+                    onClick={ this.getInfoArtista }
+                  >
+                    Entrar
+                  </button>
+                </form>)
+              : <Carregando />
+          }
+        </div>
+        <div>
+          {showResult
+            ? (
+              <ul>
+                <span>{ returnMsg }</span>
+                {singerInfos.map((info) => (
+                  <Link
+                    to={ `/album/${info.collectionId}` }
+                    data-testid={ `link-to-album-${info.collectionId}` }
+                    key={ info.collectionId }
+                  >
+                    <img src={ info.artworkUrl100 } alt={ info.collectionName } />
+                    <li>{info.collectionName}</li>
+                  </Link>
+                )) }
+              </ul>
+            )
+            : <span>{ returnMsg }</span>}
+        </div>
+      </>
     );
   }
 }
